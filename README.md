a5d7711c27bf0ca5d5c9717bcf5866735a0dc86a
iKerKa <iker48140@gmail.com>
2022-10-17 21:29:23 +0200

    InitialCode

---
+++ README.md
@@ -0,0 +1,42 @@
+# dbdRandomizer
+A randomizer for the game Dead By Daylight (DBD) that allows you to play with random perks, add-ons, offerings, and more!
+
+## How to use
+1. When the bot is added to your server, type `!dbd` to get a list of commands.
+2. Type `!dbd help` to get a list of commands.
+3. Type `!dbd help <command>` to get more information about a specific command.
+4. Type `!dbd <command>` to use a command.
+
+## Commands
+- `help` - Get an introduction to the bot
+- `commands` - Get a list of commands
+- `perk survivor` - Get a random perk from the survivors with the information about it.
+- `perk killer` - Get a random perk from the killers with the information about it.
+- `addon` - Get a random add-on with the information about it.
+- `offering` - Get a random offering with the information about it.
+- `survivor` - Get a random survivor
+- `killer` - Get a random killer
+- `survivor <name>` - Get information about a specific survivor (pending)
+- `killer <name>` - Get information about a specific killer (pending)
+- `survivorlist` - Get a list of all survivors
+- `killerlist` - Get a list of all killers
+- `perkCombos` - Get a random perk combo of 4 perks to use
+- `preMatch survivor` - Get a random pre-match lobby setup (including 4 perks (from the survivors), 1 item (with 2 add-ons), 1 offering, and 1 survivor)
+- `preMatch killer` - Get a random pre-match lobby setup (including 4 perks (from the killers), 1 item (with 2 add-ons), 1 offering, and 1 killer)
+- `item` - Get a variety-random item (f.e medkit, toolbox, etc.) with the information about it. (pending)
+    for example: `!dbd item` : green toolbox and it info
+- `item <color>` - Get a random item (f.e medkit, toolbox, etc.) with the color you specified (pending)
+    for example: `!dbd item green` : green toolbox and it info
+
+--coming soon--
+
+## Support
+If you have any questions or suggestions, feel free to join the [support server]()!
+
+## Credits
+- [DeadByDaylight](https://deadbydaylight.com/) - The game Dead By Daylight
+- [DeadByDaylight Wiki](https://deadbydaylight.gamepedia.com/Dead_by_Daylight_Wiki) - The wiki for the game Dead By Daylight
+- [DeadByDaylight API]() - The API for the game Dead By Daylight
+- [Me](Iker Morán) - The developer of this bot
+
+
---
+++ main.py
@@ -0,0 +1,106 @@
+import os
+import discord
+#the import below is for the database
+import sqlite3
+#the import below is for the api
+import requests
+import json
+import random
+# to convert the image to bytes
+import io
+
+
+intent = discord.Intents.default()
+intent.members = True
+intent.message_content = True
+
+
+# start the dbdRandomizer
+client = discord.Client(intents=intent)
+
+
+# DATABASE??
+
+# This is to check the API examples
+def get_one_random_perk():
+    # get a random perk from the api
+    response = requests.get("https://dead-by-api.herokuapp.com/api/perks/surv/random")
+    # get the json data
+    json_data = json.loads(response.text)
+
+    #print(json_data)
+   
+    # we get the name of the perk
+    # {'status': 'success', 'results': 1, 'data': [{'_id': '62d5cf44754ff04cb8ebb4e7', 'id': 18, 'name': 'This Is Not Happening'
+    perk_name = json_data['data'][0]['name']
+    # we get the description of the perk
+    perk_description = json_data['data'][0]['description']
+    # we get the icon of the perk
+    perk_icon = json_data['data'][0]['icon']
+
+    # We will show the icon of the perk on discord
+    # we need to get the image from the url
+    # we will use the requests library to get the image
+    # we will use the content attribute to get the image
+    # we will use the bytes() function to convert the image to bytes
+    # we will use the discord.File() function to convert the image to a discord file
+    
+    # First we got the icon in not bytes so we need to convert it to bytes
+    
+    perk_icon_bytes = bytes(requests.get(perk_icon).content)
+
+    # We will return perk_icon_bytes
+
+    # We now concatenate the name, description and rarity of the perk
+    perk_info = "\nName: " + perk_name + "\nDescription: " + perk_description
+
+    return perk_info, perk_icon_bytes
+
+
+
+@client.event #async function
+async def on_ready(): #when the bot is ready
+  print('We have logged in as {0.user}'.format(client))
+
+
+# When the bot receives a message
+@client.event
+async def on_message(message):
+  # if the message is from the bot user, ignore it
+  if message.author == client.user:
+    return
+
+  # if the message is !help 
+  if message.content.startswith('!help'):
+    await message.channel.send('Hello! I am the DBD Randomizer Bot. I can help you to decide your future matches in Dead by Daylight. Just type !commands to get started!')
+
+  if message.content.startswith('!test'):
+    # we take the image and the info from the function
+    perk_info, perk_image = get_one_random_perk()
+
+    # we have the image in bytes, we need to have it in utf-8
+    # we will use the io.BytesIO() function to convert the image to utf-8
+    perk_image_utf8 = io.BytesIO(perk_image)
+
+    # we will use the discord.File() function to convert the image to a discord file
+    perk_image_discord_file = discord.File(perk_image_utf8, filename="perk_image.png")
+
+    # we will send the message with the image
+    await message.channel.send(perk_info, file=perk_image_discord_file)
+    #await message.channel.send(perk_info, file=perk_image)
+  
+  
+  # Rest of the commands
+
+
+# Run the bot
+# We have to put the token in an environment variable
+
+# We can also put the token directly in the code but it is not recommended for security reasons
+client.run('MTAzMTUxOTcwNzgyOTkwNzQ4Ng.GtoNWd.Tm675WzlMwvFxlx3r9_8urm7CzGT1corn61rZs')
+
+
+
+
+
+
